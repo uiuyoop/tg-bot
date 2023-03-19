db43c613a27792ede63b52fa28ffd8769e0ac35e
uiuyoop~ <uiuyoop@gmail.com>
2023-03-19 13:55:02 +0400

    done

---
+++ main.py
@@ -0,0 +1,86 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def get_promo():
+
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+    }
+
+    url = "https://uiuyoop.github.io/siteForParser/"
+    request = requests.get(url=url, headers=headers)
+
+    soup = BeautifulSoup(request.text, "lxml")
+
+    articles_card = soup.find_all("a", class_="promo")
+
+    promo_dict = {}
+    for article in articles_card:
+        article_title = article.find("h2").text.strip()
+        article_desc = article.find("p").text.strip()
+        article_url = f'https://uiuyoop.github.io/siteForParser/{article.get("href")}'
+
+        article_id = article_url.split("/")[-1]
+        article_id = article_id[:-4]
+
+        promo_dict[article_id] = {
+            "article_title": article_title,
+            "article_url": article_url,
+            "article_desc": article_desc
+        }
+
+    with open("tgParserBot/promo_dict.json", "w") as file:
+        json.dump(promo_dict, file, indent=4, ensure_ascii=False)
+
+
+def check_promo_update():
+
+    with open("tgParserBot/promo_dict.json") as file:
+        promo_dict = json.load(file)
+
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+    }
+
+    url = "https://uiuyoop.github.io/siteForParser/"
+    request = requests.get(url=url, headers=headers)
+
+    soup = BeautifulSoup(request.text, "lxml")
+    articles_card = soup.find_all("a", class_="promo")
+
+    fresh_promo = {}
+    for article in articles_card:
+        article_url = f'https://uiuyoop.github.io/siteForParser{article.get("href")}'
+        article_id = article_url.split("/")[-1]
+        article_id = article_id[:-4]
+
+        if article_id in promo_dict:
+            continue
+        else:
+            article_title = article.find("h2").text.strip()
+            article_desc = article.find("p").text.strip()
+
+            promo_dict[article_id] = {
+                "article_title": article_title,
+                "article_url": article_url,
+                "article_desc": article_desc
+            }
+
+            fresh_promo[article_id] = {
+                "article_title": article_title,
+                "article_url": article_url,
+                "article_desc": article_desc
+            }
+
+    with open("tgParserBot/news_dict.json", "w") as file:
+        json.dump(promo_dict, file, indent=4, ensure_ascii=False)
+
+    return fresh_promo
+
+def main():
+    get_promo()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tg_bot.py
@@ -0,0 +1,57 @@
+from aiogram import Bot, Dispatcher, executor, types
+from config import token
+import json
+from main import check_promo_update
+from aiogram.dispatcher.filters import Text
+
+bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands="start")
+async def start(message: types.Message):
+    start_buttons = ["Все промо акции", "Последние 3 промо акции", "Свежие промо акции"]
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*start_buttons)
+    await message.answer("Летна промо акций", reply_markup=keyboard)    
+
+@dp.message_handler(Text(equals="Все промо акции"))
+async def get_all_promo(message: types.Message):
+    with open("tgParserBot/promo_dict.json") as file:
+        promo_dict = json.load(file)
+
+    for k, v in promo_dict.items():
+        promo = f"<u>{v['article_title']}</u>\n" \
+                f"<code>{v['article_desc']}</code>\n" \
+                f"{v['article_url']}"
+        
+        await message.answer(promo)
+
+@dp.message_handler(Text(equals="Последние 3 промо акции"))
+async def get_last_three_promo(message: types.Message):
+    with open("tgParserBot/promo_dict.json") as file:
+        promo_dict = json.load(file)
+
+    for k, v in sorted(promo_dict.items())[-3:]:
+        promo = f"<u>{v['article_title']}</u>\n" \
+                f"<code>{v['article_desc']}</code>\n" \
+                f"{v['article_url']}"
+        
+        await message.answer(promo)
+
+@dp.message_handler(Text(equals="Свежие промо акции"))
+async def get_fresh_promo(message: types.Message):
+    fresh_promo = check_promo_update()
+
+    if len(fresh_promo) >= 1:
+        for k, v in sorted(fresh_promo.items()):
+            promo = f"<u>{v['article_title']}</u>\n" \
+                f"<code>{v['article_desc']}</code>\n" \
+                f"{v['article_url']}"
+        
+        await message.answer(promo)
+    else:
+        await message.answer("Пока что нет свежих промо акций...")
+
+if __name__ == "__main__":
+    # Запускаем бота
+    executor.start_polling(dp)
