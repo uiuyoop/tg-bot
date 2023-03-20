c92619a2a217bae186ccc75214f38779d939eb35
uiuyoop~ <uiuyoop@gmail.com>
2023-03-20 14:42:35 +0400

    add coms

--- main.py
+++ main.py
@@ -1,44 +1,52 @@
+# Импортируем библиотеки и модули
 import requests
 from bs4 import BeautifulSoup
 import json
 
+# Пишем функцию получения элементов
 def get_promo():
-
+    # Забираем хедер запроса 
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
     }
-
+    # Назначаем URL страницы для парсинга
     url = "https://uiuyoop.github.io/for-parser/"
+    # Пишем запрос на получения от страницы
     request = requests.get(url=url, headers=headers)
-
+    # Используем bs4 и lxml для парсинга страницы
     soup = BeautifulSoup(request.text, "lxml")
-
+    # Парсим все элементы "a" с нужным классом
     articles_card = soup.find_all("a", class_="article-card")
-
+    # Создаем список для записи элементов
     promo_dict = {}
+    # Проводим цикл по всем "a" и находим нужные элементы для отправики в чат
     for article in articles_card:
+        # Заголовки
         article_title = article.find("h2", class_="article-card-title").text.strip()
+        # Описание
         article_desc = article.find("p").text.strip()
+        # Гиперссылку на страницу
         article_url = f'https://uiuyoop.github.io/for-parser/{article.get("href")}'
-
+        # Пробегаем по id с последнего места
         article_id = article_url.split("/")[-1]
+        # Делаем срез
         article_id = article_id[:-4]
-
+        # Заносим спарсенные элементы в список JSON форматом
         promo_dict[article_id] = {
             "article_title": article_title,
             "article_url": article_url,
             "article_desc": article_desc
         }
-
+    # Записываем JSON файл
     with open("tg-bot/promo_dict.json", "w") as file:
         json.dump(promo_dict, file, indent=4, ensure_ascii=False)
 
-
+# Пишем функцию для проверки новых акций
 def check_promo_update():
-
+    # Загружаем, записанный JSON файл
     with open("tg-bot/promo_dict.json") as file:
         promo_dict = json.load(file)
-
+    
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
     }
@@ -48,39 +56,40 @@ def check_promo_update():
 
     soup = BeautifulSoup(request.text, "lxml")
     articles_card = soup.find_all("a", class_="article-card")
-
+    # Вводим новый список, свежих акций
     fresh_promo = {}
+    # Проходим циклом по id элементам
     for article in articles_card:
         article_url = f'https://uiuyoop.github.io/for-parser/{article.get("href")}'
         article_id = article_url.split("/")[-1]
         article_id = article_id[:-4]
-
+        # Пишем условие, если такой id есть в списке, пропускаем
         if article_id in promo_dict:
             continue
+        # Если нет, то парсим новый элемент
         else:
             article_title = article.find("h2", class_="article-card-title").text.strip()
             article_desc = article.find("p").text.strip()
-
+            # Записываем id элементов в промо список
             promo_dict[article_id] = {
                 "article_title": article_title,
                 "article_url": article_url,
                 "article_desc": article_desc
             }
-
+            # Записываем id элементов в свежий промо список
             fresh_promo[article_id] = {
                 "article_title": article_title,
                 "article_url": article_url,
                 "article_desc": article_desc
             }
-
-    with open("tg-bot/news_dict.json", "w") as file:
+    # Записываем JSON файл
+    with open("tg-bot/promo_dict.json", "w") as file:
         json.dump(promo_dict, file, indent=4, ensure_ascii=False)
-
+    # Возвращаем список свещих акций
     return fresh_promo
-
+# Запускаем функцию для получения акций
 def main():
     get_promo()
-
-
+# Запускаем код
 if __name__ == '__main__':
     main()
--- tg_bot.py
+++ tg_bot.py
@@ -3,29 +3,32 @@ from config import token
 import json
 from main import check_promo_update
 from aiogram.dispatcher.filters import Text
-
+# Иницииурем бота
 bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
-
+# Создаем команду старт
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
+    # Создаем кнопки
     start_buttons = ["Все промо акции", "Последние 3 промо акции", "Свежие промо акции"]
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # Добавляем кнопку старт в бота
     keyboard.add(*start_buttons)
     await message.answer("Летна промо акций", reply_markup=keyboard)    
-
+# Создаем кнопку и логику получения всех акций с сайта
 @dp.message_handler(Text(equals="Все промо акции"))
 async def get_all_promo(message: types.Message):
+    # Загрузаем JSON файл
     with open("tg-bot/promo_dict.json") as file:
         promo_dict = json.load(file)
-
+    # Проводим цикл по списку
     for k, v in promo_dict.items():
         promo = f"<u>{v['article_title']}</u>\n" \
                 f"<code>{v['article_desc']}</code>\n" \
                 f"{v['article_url']}"
-        
+        # Выводим ответом в UI 
         await message.answer(promo)
-
+# Создаем кнопку и логику получения трех последних акций с сайта
 @dp.message_handler(Text(equals="Последние 3 промо акции"))
 async def get_last_three_promo(message: types.Message):
     with open("tg-bot/promo_dict.json") as file:
@@ -37,11 +40,11 @@ async def get_last_three_promo(message: types.Message):
                 f"{v['article_url']}"
         
         await message.answer(promo)
-
+# Создаем кнопку и логику получения новых акций с сайта
 @dp.message_handler(Text(equals="Свежие промо акции"))
 async def get_fresh_promo(message: types.Message):
     fresh_promo = check_promo_update()
-
+    # Создаем условия, при которых, если в списке fresh_promo добавляется 1 и больше элементов, то проходим цикллом по ним и отправляем ответ в бота
     if len(fresh_promo) >= 1:
         for k, v in sorted(fresh_promo.items()):
             promo = f"<u>{v['article_title']}</u>\n" \
@@ -50,6 +53,7 @@ async def get_fresh_promo(message: types.Message):
         
         await message.answer(promo)
     else:
+        # Если нет, то отправляем сообщение
         await message.answer("Пока что нет свежих промо акций...")
 
 if __name__ == "__main__":
